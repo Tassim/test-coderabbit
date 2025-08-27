8b5846cebd12df6d6cd0f894607bf8999abb2231
Tassia Shibuya <tassiashibuya@mac.lan>
2025-08-27 10:43:40 -0700

    changed remove func

--- whatever.js
+++ whatever.js
@@ -11,3 +11,19 @@ const deleteItem = (array, item) => {
     }
     return array;
 };
+
+const updateItem = (array, oldItem, newItem) => {
+    const index = array.indexOf(oldItem);
+    if (index > -1) {
+        array[index] = newItem;
+    }
+    return array;
+}
+
+const getItem = (array, item) => {
+    const index = array.indexOf(item);
+    return index > -1 ? array[index] : null;
+};
+
+// Exporting functions for use in other modules
+export { addItem, deleteItem, updateItem, getItem };
